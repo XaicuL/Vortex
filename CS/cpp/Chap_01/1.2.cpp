f2c12050f3f49894c97fedd730c11d7df7eb18fd
XaicuL <hyunjun050915@gmail.com>
2026-06-29 21:57:10 +0900

    add : chap_01 - 2

--- CS/cpp/Chap_01/1.2.cpp
+++ CS/cpp/Chap_01/1.2.cpp
@@ -1,3 +1,14 @@
-//
-// Created by Jeon HyunJun on 6/29/26.
-//
+// 은 주석을 의미한다.
+// 주석은 다음과 같이 사용 할 수 있다.
+
+#include <iostream>
+
+int main() {
+
+    std::cout << "Hello world"; //이건 Hello world를 출력할 것. <-- 이런 식으로 사용한다.
+    return 0;
+}
+
+/** 주석은 이렇게도 사용할 수 있는데,
+이건 주로 장문이거나 한 줄로 끝나지 않을 때 사용한다.
+**/
